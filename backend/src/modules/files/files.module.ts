b5ce3f6109163be561838cfd96ef0b61edb0d818
passing7777 <2998256503@qq.com>
2026-06-12 17:43:09 +0800

    更新

--- backend/src/modules/files/files.module.ts
+++ backend/src/modules/files/files.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { StorageModule } from '../../storage/storage.module';
 
 @Module({
+  imports: [StorageModule],
   controllers: [FilesController],
   providers: [FilesService],
   exports: [FilesService],
--- backend/src/modules/shares/shares.module.ts
+++ backend/src/modules/shares/shares.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SharesController } from './shares.controller';
 import { SharesService } from './shares.service';
+import { StorageModule } from '../../storage/storage.module';
 
 @Module({
+  imports: [StorageModule],
   controllers: [SharesController],
   providers: [SharesService],
   exports: [SharesService],
